fba9809165dd53dbc227f8f576883cf57e49045c
Chance Snow <git@chancesnow.me>
2024-02-25 13:20:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DBPF
+D library to read and write [Database Packed File](https://www.wiki.sc4devotion.com/index.php?title=DBPF) (DBPF) archives.
---
+++ src/package.d
@@ -0,0 +1,11 @@
+/// Read and write Database Packed File (DBPF) archives.
+///
+/// See_Also: $(UL
+///   $(LI <a href="https://www.wiki.sc4devotion.com/index.php?title=DBPF">DBPF</a> (SC4D Encyclopedia))
+///   $(LI <a href="https://www.wiki.sc4devotion.com/index.php?title=List_of_File_Formats">List of File Formats</a> (SC4D Encyclopedia))
+/// )
+///
+/// Authors: Chance Snow
+/// Copyright: Copyright © 2024 Chance Snow. All rights reserved.
+/// License: MIT License
+module dbpf;
